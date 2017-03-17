3b65df66adb36c9121407400d4dc7575fbfe250f
czn <2985697630@qq.com>
2017-03-17 16:14:52 +0800

    u

--- app/src/main/java/me/yifeiyuan/wilfulrvsample/MainActivity.java
+++ app/src/main/java/me/yifeiyuan/wilfulrvsample/MainActivity.java
@@ -1,13 +1,20 @@
 package me.yifeiyuan.wilfulrvsample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private android.widget.TableLayout tbl;
+    private android.support.v4.view.ViewPager vp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.vp = (ViewPager) findViewById(R.id.vp);
+        this.tbl = (TableLayout) findViewById(R.id.tbl);
     }
 }
